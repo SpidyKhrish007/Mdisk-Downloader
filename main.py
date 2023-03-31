95500f6655a01f8823ce541e9f5194dce35cd0ea
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-03-31 21:59:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from split import TG_SPLIT_SIZE
 
 # app
 bot_token = os.environ.get("TOKEN", "6008002657:AAErQz7_XpZgoha7XXC0jh1aq-_sIQInnow") 
-api_hash = os.environ.get("HASH", "edb811cb83c768a50060c02f659273e7") 
-api_id = os.environ.get("ID", "28559411")
+api_hash = os.environ.get("HASH", "19f8365d98fb11c9cd6c1eaa8b1fa4b8") 
+api_id = os.environ.get("ID", "23830477")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
 
 # preiumum
